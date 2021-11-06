777f8c7f2976c05ba073891276c002f71bb6bee4
sahil111020 <sahil111020@gmail.com>
2021-11-06 13:54:27 +0000

    faq fixed

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image9 from '../images/9.png'
 
 function FaqSection() {
     
+    const [isOpen, setIsOpen] = useState(false);
+
+    function closeingOtherTags(){
     const details = Array.from(document.querySelectorAll("details"));
 
     details.forEach(targetDetail => {
@@ -14,6 +17,7 @@ function FaqSection() {
             })
         })
     })
+    }
 
     return (
         <div className="flex flex-col bg-yellow-400 w-full mt-5">
@@ -22,7 +26,7 @@ function FaqSection() {
                 
                 <img src={Image9} alt="" className="h-44 md:h-52 lg:h-56 xl:h-72 2xl:h-80 m-auto md:mr-14 lg:mr-20 xl:mr-28 2xl:mr-40"/>
 
-                <div className="p-5 w-full px-6 md:ml-14 lg:ml-20 xl:ml-28 2xl:ml-40">
+                <div className="p-5 w-full px-6 md:ml-14 lg:ml-20 xl:ml-28 2xl:ml-40"  onClick={closeingOtherTags}>
                     <details className="cursor-pointer border-4 border-black p-2 md:p-4 rounded-lg mt-3">
                         <summary className="list-none">Lorem ipsum dolor.</summary>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore minima ipsum impedit magnam dicta libero ratione. Sapiente ipsum temporibus natus, deserunt corrupti eligendi assumenda vero placeat quas consectetur eveniet? Veniam!</p>
